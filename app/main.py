2002d19fa0e55dfb8d6cd3a0ba228163aa660782
Steven <tetraman4arms@gmail.com>
2025-07-09 01:40:59 -0400

    main.py update

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
 
-from .analyzer import analyze_text, rewrite_text
-from .schemas import AnalyzeRequest, AnalyzeResponse, SuggestResponse, RewriteRequest, RewriteResponse
+from analyzer import analyze_text, rewrite_text
+from schemas import AnalyzeRequest, AnalyzeResponse, SuggestResponse, RewriteRequest, RewriteResponse
 
 # --- Schemas --- #
 
